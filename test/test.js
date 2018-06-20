ebb0a5932b218c2bf323bd01c96f8a8d5388678b
Lauren Zou <zou.lauren@gmail.com>
2018-06-20 08:54:44 -0400

    Add tests to ensure that using the same seed returns the same values.

--- test/test.js
+++ test/test.js
@@ -3,19 +3,34 @@ var assert = require('assert');
 var Utils = require('../js/utils');
 
 describe('Utils', function() {
-  var setup;
+  var seeds;
+  var wordBank;
 
   before(function() {
-    var seed = 'hello-world';
-    var wordBank = JSON.parse(fs.readFileSync('data/words.json', 'utf8'));
-    setup = new Utils(seed, wordBank);
+    seeds = [
+      'hello-world',
+      'lemons-are-sweet',
+      'i-like-to-eat',
+      'kittens-are-cute'
+    ];
+    wordBank = JSON.parse(fs.readFileSync('data/words.json', 'utf8'));
   });
 
-  it('#getRandomNumber', function() {
-    assert.equal(setup.getRandomNumber(''), 0.6122790858798908);
+  it('#getRandomNumber returns a random number', function() {
+    var utils = new Utils(seeds[0], wordBank);
+    assert.equal(utils.getRandomNumber(''), 0.6122790858798908);
   });
 
-  it('#getWords', function() {
+  it('#getRandomNumber returns the same random number for the same seed', function() {
+    for (var i = 0; i < seeds.length; i++) {
+      var utils1 = new Utils(seeds[i], wordBank);
+      var utils2 = new Utils(seeds[i], wordBank);
+      assert.equal(utils1.getRandomNumber(i.toString()), utils2.getRandomNumber(i.toString()));
+    }
+  });
+
+  it('#getWords returns a set of unique words', function() {
+    var utils = new Utils(seeds[0], wordBank);
     var expectedSet = new Set([
       'Crust',
       'Ergonomic',
@@ -43,6 +58,14 @@ describe('Utils', function() {
       'Frog',
       'Cone'
     ]);
-    assert.deepEqual(setup.getWords(), expectedSet);
-  })
+    assert.deepEqual(utils.getWords(), expectedSet);
+  });
+
+  it('#getWords returns the same set of unique words for the same seed', function() {
+    for (var i = 0; i < seeds.length; i++) {
+      var utils1 = new Utils(seeds[i], wordBank);
+      var utils2 = new Utils(seeds[i], wordBank);
+      assert.deepEqual(utils1.getWords(), utils2.getWords());
+    }
+  });
 });
