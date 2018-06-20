b26c0efe5bc2444e7579875514da4e02140c813f
Lauren Zou <zou.lauren@gmail.com>
2018-06-20 09:19:36 -0400

    Use seed from URL parameter or a random seed.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,31 @@ function toggleView() {
 }*/
 
 $(document).ready(function() {
-  var seed = 'hello-world';
-  
+  var seed = getSeed();
+
   getWordBank(WORD_BANK_FILE_NAME).done(function(wordBank) {
     var utils = new Utils(seed, wordBank);
     var words = utils.getWords(GAME_WORD_COUNT);
     var game = new Game(words);
   });
 });
+
+/**
+ * Gets the seed from the URL parameter ?seed=.
+ *
+ * If the seed does not exist in the URL parameter, generate a new seed.
+ **/
+function getSeed() {
+  var parameters = window.location.search.slice(1).split('&');
+  for (var i = 0; i < parameters.length; i++) {
+    var keyValuePair = parameters[i].split('=');
+    if (keyValuePair[0] === 'seed') {
+      return keyValuePair[1];
+    }
+  }
+  // TODO: Generate a random seed.
+  return 'hello-world';
+}
 
 /** Returns an array of words read from a JSON file. */
 function getWordBank(fileName) {
