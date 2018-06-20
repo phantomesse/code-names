b35f367e489121dfbdcb30d5809932bc8c466550
Lauren Zou <zou.lauren@gmail.com>
2018-06-20 00:36:57 -0400

    Use a seed to generate random words.

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,50 @@
+const ROW_WORD_COUNT = 5;
+const COLUMN_WORD_COUNT = 5;
+const GAME_WORD_COUNT = ROW_WORD_COUNT * COLUMN_WORD_COUNT;
+
 /** Sets up the Code Names game by generating a random Game with Cards based on a seed. */
 (function() {
   var Setup = (function() {
-    var Setup = function(wordsFileName) {
-      this.wordsFileName = wordsFileName;
-      console.log('hi');
+    var Setup = function(seed, wordBank) {
+
+      /**
+       * Seed used to ensure that randomly generating Game and Cards are always randomly generated
+       * in the same way.
+       **/
+      this.seed = seed;
+
+      /** Array of words that can be used. */
+      this.wordBank = wordBank;
     };
+
+    /**
+     * Generates a random number from the seed.
+     *
+     * https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+     **/
+    Setup.prototype.getRandomNumber = function getRandomNumber(seed) {
+      seed += this.seed;
+      var numString = '';
+      for (var i = 0; i < seed.length; i++) {
+        var characterNum = seed.charCodeAt(i) * (i + 1);
+        numString += characterNum;
+      }
+      var x = Math.sin(parseInt(numString) + 1) * 10000;
+      return x - Math.floor(x);
+    }
 
     /** Reads a number of random words for the Cards. */
     Setup.prototype.getWords = function getWords() {
-      return this.wordsFileName;
+      var words = new Set();
+
+      var i = 0;
+      while (words.size < GAME_WORD_COUNT) {
+        i++;
+        var randomIndex = parseInt(this.getRandomNumber(i.toString()) * 255);
+        words.add(this.wordBank[randomIndex]);
+      }
+
+      return words;
     };
 
     return Setup;
--- test/test.js
+++ test/test.js
@@ -1,13 +1,48 @@
+var fs = require('fs');
 var assert = require('assert');
 var Setup = require('../js/setup');
 
-describe("Testing Code Names", function () {
-  before(function () {
+describe('Setup', function() {
+  var setup;
 
+  before(function() {
+    var seed = 'hello-world';
+    var wordBank = JSON.parse(fs.readFileSync('data/words.json', 'utf8'));
+    setup = new Setup(seed, wordBank);
   });
 
-  it("Setup.", function () {
-    var setup = new Setup('blah');
-    assert.equal(setup.getWords(), 'blah');
+  it('#getRandomNumber', function() {
+    assert.equal(setup.getRandomNumber(''), 0.6122790858798908);
   });
+
+  it('#getWords', function() {
+    var expectedSet = new Set([
+      'Crust',
+      'Ergonomic',
+      'Deep',
+      'Cuff',
+      'Archaeologist',
+      'Ear',
+      'Flagpole',
+      'Fringe',
+      'Chef',
+      'Fix',
+      'Diver',
+      'Banana',
+      'Buy',
+      'Aircraft',
+      'Curtain',
+      'Flu',
+      'Battery',
+      'Garbage',
+      'Broken',
+      'Dart',
+      'Dawn',
+      'Cheerleader',
+      'Birthday',
+      'Frog',
+      'Cone'
+    ]);
+    assert.deepEqual(setup.getWords(), expectedSet);
+  })
 });
