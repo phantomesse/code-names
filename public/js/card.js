2f2bf2d8a5df717a2a18f8ad28930e148585f3c7
Lauren Zou <laurenzou@google.com>
2018-07-17 12:01:10 -0400

    Add card colors on flip.

--- public/js/card.js
+++ public/js/card.js
@@ -6,33 +6,35 @@ class Card {
     this.sessionName = sessionName;
     this.word = word;
     this.type = type;
-    this.element = this._createElement(isFlipped);
+    this.isFlipped = isFlipped;
+    this.element = this._createElement();
+
+    if (this.isFlipped) this._flipCard(false);
+
+    var self = this;
+    socket.on('updated flipped word', function(word, sessionName) {
+      if (sessionName === self.sessionName && word === self.word) {
+        self._flipCard(false);
+      }
+    });
   }
 
-  _createElement(isFlipped) {
+  _createElement() {
+    var self = this;
     var element = $('<button>')
       .addClass('card')
       .text(this.word)
+      .click(() => self._flipCard(true))
       .appendTo('.game');
-
-    if (isFlipped) element.addClass('flipped');
-
-    var self = this;
-    element.click(function() {
-      var data = {
-        'word': self.word,
-        'sessionName': self.sessionName
-      };
-      socket.emit('updated flipped word', self.word, self.sessionName);
-      element.addClass('flipped');
-    });
-
-    socket.on('updated flipped word', function(word, sessionName) {
-      if (sessionName === self.sessionName && word === self.word) {
-        element.addClass('flipped');
-      }
-    });
     return element;
+  }
+
+  _flipCard(shouldEmitEvent) {
+    if (shouldEmitEvent) {
+      socket.emit('updated flipped word', this.word, this.sessionName);
+    }
+
+    this.element.addClass('flipped ' + this.type);
   }
 }
 
