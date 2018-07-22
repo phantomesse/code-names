6168f9cdae7037a450c16c3c00371fd22d5ee7e4
Lauren Zou <zou.lauren@gmail.com>
2018-07-21 21:04:40 -0400

    Add card states.

--- public/js/components/card.js
+++ public/js/components/card.js
@@ -3,13 +3,47 @@
 /** Contains information and HTML element pertaining to a card. */
 class Card {
   constructor(data) {
-    this.element = Card._createElement(data);
+    this.word = data.word;
+    this.state = data.isFlipped ? _CardState.FLIPPED : _CardState.UNFLIPPED;
+    this.element = this._createElement(this.word);
   }
 
   /** Creates an HTML element, but doesn't render it on the screen. */
-  static _createElement(data) {
+  _createElement() {
+    const self = this;
     return $('<button>')
       .addClass('card')
-      .text(data.word);
+      .text(this.word)
+      .click(() => self._handleClick());
+  }
+
+  /** Handles card click. */
+  _handleClick() {
+    switch (this.state) {
+      case _CardState.UNFLIPPED:
+        this._updateState(_CardState.SELECTED);
+        break;
+      case _CardState.SELECTED:
+        this._updateState(_CardState.FLIPPED);
+        break;
+      case _CardState.FLIPPED:
+        return;
+    }
+  }
+
+  /** Updates the state of the card. */
+  _updateState(state) {
+    this.state = state;
+    for (const key in _CardState) {
+      const state = _CardState[key];
+      if (state === this.state) this.element.addClass(state);
+      else this.element.removeClass(state);
+    }
   }
 }
+
+const _CardState = Object.freeze({
+  UNFLIPPED: 'unflipped',
+  SELECTED: 'selected',
+  FLIPPED: 'flipped'
+});
