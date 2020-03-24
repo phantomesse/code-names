3ee30fdec79df377f643ceabc72e0b02ee7721d3
Lauren Zou <zou.lauren@gmail.com>
2020-03-24 09:41:52 -0400

    Add cards left.

--- frontend/game-view/game-view.ts
+++ frontend/game-view/game-view.ts
@@ -1,4 +1,9 @@
-import { Component, HostBinding } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 import DataController from '../controllers/data-controller';
 import { Router } from '@angular/router';
 import CardModel from '../models/card-model';
@@ -6,14 +11,21 @@ import CardModel from '../models/card-model';
 @Component({
   selector: 'game-view',
   templateUrl: './game-view.html',
-  styleUrls: ['./game-view.scss']
+  styleUrls: ['./game-view.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GameViewComponent {
   sessionName: string;
   cards: CardModel[] = [];
   isSpymasterView: boolean;
+  redCardsLeftMessage: string;
+  blueCardsLeftMessage: string;
 
-  constructor(private _router: Router, dataController: DataController) {
+  constructor(
+    private _router: Router,
+    dataController: DataController,
+    changeDetector: ChangeDetectorRef
+  ) {
     const parameters = this._router.url.substr(1).split('/');
     this.sessionName = parameters[0];
     this.isSpymasterView = parameters.length > 1;
@@ -22,6 +34,15 @@ export class GameViewComponent {
       this.cards = cards;
       this.accentColor =
         cards.filter(card => card.type === 'red').length === 9 ? 'red' : 'blue';
+      this.redCardsLeftMessage = GameViewComponent.getCardLeftMessage(
+        cards,
+        'red'
+      );
+      this.blueCardsLeftMessage = GameViewComponent.getCardLeftMessage(
+        cards,
+        'blue'
+      );
+      changeDetector.markForCheck();
     });
   }
 
@@ -32,5 +53,14 @@ export class GameViewComponent {
     this._router.navigateByUrl(
       '/' + this.sessionName + (this.isSpymasterView ? '' : '/spy')
     );
+  }
+
+  static getCardLeftMessage(cards: CardModel[], cardType: string) {
+    const cardCount = cards.filter(
+      card => !card.isFlipped && card.type === cardType
+    ).length;
+    return `${cardCount} ${cardType} ${
+      cardCount === 1 ? 'card' : 'cards'
+    } left`;
   }
 }
